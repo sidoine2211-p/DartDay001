1feb73940ec7dee5b14dc697e92bbf4dd962bcdf
sidoine2211-p <sidoineamoussouy@gmaile.com>
2025-01-27 20:36:47 +0100

    Fonction pour calculer la surface de base d'un pavé

---
+++ paveMagique3.dart
@@ -0,0 +1,18 @@
+void main() {
+
+ calculSurfaceBasePave( 10, 20); 
+ 
+  
+}
+
+
+double calculSurfaceBasePave(double longueur, double largeur) {
+  
+  double surface = longueur*largeur;
+  
+  print(surface);
+    
+  return surface;
+  
+  
+}
