e578cf5f783ce54c7379d604eaeca209c2310acd
sidoine2211-p <sidoineamoussouy@gmaile.com>
2025-01-27 20:57:00 +0100

    Fonction pour calculer le périmètre de la base du pavé droit

---
+++ paveMagique3.dart
@@ -0,0 +1,15 @@
+void main() {
+
+ calculPerimetreBasePave(20,15); 
+ 
+  
+}
+
+double calculPerimetreBasePave(double longueur, double largeur) {
+  
+
+  double perimetreBase = (2*longueur) + (2*largeur);
+  print(perimetreBase);
+  return perimetreBase;
+    
+}
