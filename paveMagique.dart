8332b3c02f83fe4140e5881a7e3e79dafa450146
sidoine2211-p <sidoineamoussouy@gmaile.com>
2025-01-27 19:49:54 +0100

    premier commit dart

---
+++ paveMagique.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  calculVolume(10.5,10,20);
+}
+
+double calculVolume(double longueur, double largeur, double hauteur) {
+  
+  double volume = longueur*largeur*hauteur; 
+    print(volume);
+    return volume;  
+}
